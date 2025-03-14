750fe445e52b3b894cecde834131f38639349c05
iFuxu <109459544+iFuxu@users.noreply.github.com>
2025-03-14 16:36:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,34 +8,34 @@ import re
 """
 
 # 阅读次数 默认120次/60分钟
-READ_NUM = int(os.getenv('READ_NUM') or 120)
+READ_NUM = int(os.getenv('READ_NUM') 或 120)
 # 需要推送时可选，可选pushplus、wxpusher、telegram
-PUSH_METHOD = "" or os.getenv('PUSH_METHOD')
+PUSH_METHOD = "" 或 os.getenv('PUSH_METHOD')
 # pushplus推送时需填
-PUSHPLUS_TOKEN = "" or os.getenv("PUSHPLUS_TOKEN")
+PUSHPLUS_TOKEN = "" 或 os.getenv("PUSHPLUS_TOKEN")
 # telegram推送时需填
-TELEGRAM_BOT_TOKEN = "" or os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = "" or os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_BOT_TOKEN = "" 或 os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = "" 或 os.getenv("TELEGRAM_CHAT_ID")
 # wxpusher推送时需填
-WXPUSHER_SPT = "" or os.getenv("WXPUSHER_SPT")
+WXPUSHER_SPT = "" 或 os.getenv("WXPUSHER_SPT")
 # read接口的bash命令，本地部署时可对应替换headers、cookies
 curl_str = os.getenv('WXREAD_CURL_BASH')
 
 # headers、cookies是一个省略模版，本地或者docker部署时对应替换
 cookies = {
-    'RK': 'oxEY1bTnXf',
-    'ptcz': '53e3b35a9486dd63c4d06430b05aa169402117fc407dc5cc9329b41e59f62e2b',  
-    'pac_uid': '0_e63870bcecc18',  
-    'iip': '0',
-    '_qimei_uuid42': '183070d3135100ee797b08bc922054dc3062834291',
-    'wr_avatar': 'https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FeEOpSbFh2Mb1bUxMW9Y3FRPfXwWvOLaNlsjWIkcKeeNg6vlVS5kOVuhNKGQ1M8zaggLqMPmpE5qIUdqEXlQgYg%2F132',  
-    'wr_gender': '0',  
+    'RK': 'oxEY1bTnXf'，
+    'ptcz': '53e3b35a9486dd63c4d06430b05aa169402117fc407dc5cc9329b41e59f62e2b'，
+    'pac_uid': '0_e63870bcecc18'，  
+    'iip': '0'，
+    '_qimei_uuid42': '183070d3135100ee797b08bc922054dc3062834291'，  
+    'wr_avatar': 'https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FeEOpSbFh2Mb1bUxMW9Y3FRPfXwWvOLaNlsjWIkcKeeNg6vlVS5kOVuhNKGQ1M8zaggLqMPmpE5qIUdqEXlQgYg%2F132'，    
+    'wr_gender': '0'，  
 }
 
 headers = {
-    'accept': 'application/json, text/plain, */*',
-    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ko;q=0.5',  
-    'baggage': 'sentry-environment=production,sentry-release=dev-1730698697208,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=1ff5a0725f8841088b42f97109c45862',  
+    'accept': 'application/json, text/plain, */*'，
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ko;q=0.5'，  
+    'baggage': 'sentry-environment=production,sentry-release=dev-1730698697208,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=1ff5a0725f8841088b42f97109c45862'，  
     'user-agent': 'sentry-environment=production,sentry-release=dev-1738835404736,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=d9923b9398aa484e9ec4f6b78148f0e8' \,  
 }
 
@@ -44,20 +44,20 @@ headers = {
 建议保留区域|默认读三体，其它书籍自行测试时间是否增加
 """
 data = {
-    "appId": "wb182564874663h1964571299",
-    "b": "a57325c05c8ed3a57224187",  
-    "c": "5b832ac0313d5b8add2af21",  
-    "ci": 317,  
-    "co": 338,  
-    "sm": "第315章杨涟(2)杨涟没有，所以不行。",
-    "pr": 100,  
-    "rt": 10,  
-    "ts": 1741940943046,  
-    "rn": 53,  
-    "sg": "d79625180dfe7f45008a4f7376c3efef636c212ce00759ff788d729d9b56aa43",  
-    "ct": 1741940943,  
-    "ps": "d00325f07a61fdacg019a69",  
-    "pc": "3e8322307a61fdadg011215",  
+    "appId": "wb182564874663h1964571299"，
+    "b": "a57325c05c8ed3a57224187"，  
+    "c": "5b832ac0313d5b8add2af21"，  
+    "ci": 317，  
+    "co": 338，  
+    "sm": "第315章杨涟(2)杨涟没有，所以不行。"，
+    "pr": 100，  
+    "rt": 10，  
+    "ts": 1741940943046，  
+    "rn": 53，  
+    "sg": "d79625180dfe7f45008a4f7376c3efef636c212ce00759ff788d729d9b56aa43"，  
+    "ct": 1741940943，  
+    "ps": "d00325f07a61fdacg019a69"，  
+    "pc": "3e8322307a61fdadg011215"，  
 }
 
 
